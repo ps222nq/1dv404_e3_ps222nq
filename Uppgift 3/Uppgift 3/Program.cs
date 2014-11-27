662a54bb3d86323b367e1d12903fc3f09d0ef861
Pepyn Swagemakers <ps222nq@student.lnu.se>
2014-11-27 12:13:56 +0100

    uppgift 3

---
+++ Uppgift 3/Uppgift 3/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Uppgift_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("enter string");
+            string input = Console.ReadLine();
+
+            char[] inputArray = input.ToCharArray();
+            Array.Reverse(inputArray);
+            string reversed = new String(inputArray);
+
+            if (reversed == input) { Console.WriteLine( input + " är en palindrom!"); }
+        }
+    }
+}
