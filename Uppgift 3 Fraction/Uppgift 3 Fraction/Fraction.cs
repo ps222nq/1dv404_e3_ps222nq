f9f93e715d3153c03accfcf0db1d8a6ae6294d33
Pepyn Swagemakers <ps222nq@student.lnu.se>
2014-11-30 09:15:14 +0100

    Finishing up 3b

--- Uppgift 3 Fraction/Uppgift 3 Fraction/Fraction.cs
+++ Uppgift 3 Fraction/Uppgift 3 Fraction/Fraction.cs
@@ -37,24 +37,59 @@ namespace Uppgift_3_Fraction
             Denominator = den;
         }
 
-        public bool IsNegative()
+        public bool IsNegative(Fraction f)
         {
+            if(Numerator < 0 || Denominator < 0){
+                return true;
+            }
+            else {
+                return false; 
+            }
+        }
+
+        public bool IsEqual(Fraction fOne, Fraction fTwo)
+        {
+            if (fOne.Numerator / fOne.Denominator == fTwo.Numerator / fTwo.Denominator) {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
 
         }
 
-        public bool IsEqual()
+        public static Fraction Add(Fraction fOne, Fraction fTwo) 
         {
+            int resultNum;
+            int resultDen;
 
-        }
+            if (fOne.Denominator == fTwo.Denominator)
+            {
+                resultNum = fOne.Numerator + fTwo.Denominator;
+                resultDen = fOne.Numerator;
+            }
+            else
+            {
+                resultNum = (fOne.Numerator * fTwo.Denominator) + (fTwo.Numerator * fOne.Denominator);
+                resultDen = fOne.Denominator * fTwo.Denominator;
+            }
 
-        public static Fraction Add() 
-        {
+            return new Fraction(resultNum, resultDen);
         
         }
 
-        public static Fraction Multiply()
+        public static Fraction Multiply(Fraction fOne, Fraction fTwo)
         {
+            int resultNum = fOne.Numerator * fTwo.Denominator;
+            int resultDen = fOne.Denominator * fTwo.Denominator;
 
+            return new Fraction(resultNum, resultDen);
+        }
+
+        public override string ToString()
+        {
+            return string.Format("{0}/{1}", Numerator, Denominator);
         }
     }
 }
--- Uppgift 3 Fraction/Uppgift 3 Fraction/Program.cs
+++ Uppgift 3 Fraction/Uppgift 3 Fraction/Program.cs
@@ -10,6 +10,38 @@ namespace Uppgift_3_Fraction
     {
         static void Main(string[] args)
         {
+            Fraction frac1 = new Fraction(1,4);
+            Fraction frac2 = new Fraction(2,8);
+
+            //Test 1: IsEqual()
+            Console.WriteLine("Test 1: Att jämföra 2 bråk med IsEqual():");
+            Console.WriteLine("Är " + frac1.ToString() + " samma som " + frac2.ToString() + "?");
+            Console.WriteLine(frac1.IsEqual(frac1, frac2));
+            Console.WriteLine();
+
+            //Test 2: IsNegative()
+            Console.WriteLine("Test 2: Att kontrollera för negativa tal med IsNegative():");
+            Console.WriteLine("Är " + frac1.ToString() + " negativ?");
+            Console.WriteLine(frac1.IsNegative(frac1));
+            Console.WriteLine();
+            
+            //Test 3: ToString()
+            Console.WriteLine("Test 3: skriva ut bråktal som sträng med ToString()");
+            Console.WriteLine("Bråktalet frac1: " + frac1.ToString());
+            Console.WriteLine("Bråktalet frac2: " + frac2.ToString());
+            Console.WriteLine();
+
+            //Test 4: Add()
+            Console.WriteLine("Test 4: addition av bråktal med Add()");
+            Fraction frac3 = Fraction.Add(frac1, frac2);
+            Console.WriteLine(frac1.ToString() + " + " + frac2.ToString() + " = " + frac3.ToString());
+            Console.WriteLine();
+
+            //Test 5: Multiply()
+            Console.WriteLine("Test 5: multiplikation av bråktal med Multiply()");
+            Fraction frac4 = Fraction.Multiply(frac1, frac3);
+            Console.WriteLine(frac1.ToString() + " * " + frac2.ToString() + " = " + frac4.ToString());
+            Console.WriteLine();
         }
     }
 }
